419ae810b29de0dc6449998fa045c52bf23b769c
lunar11120 <lunar11120@gmail.com>
2025-07-30 16:43:35 +0700

    Commit 30 July : Add spring Add Oauth2 Project ---------------------------------------------- - Change main run Application Servlet

--- src/main/java/com/am/springLogin/SpringLoginApplication.java
+++ src/main/java/com/am/springLogin/SpringLoginApplication.java
@@ -20,6 +20,7 @@ https://www.youtube.com/watch?v=2WNjmT2z7c4&t=177s
     
 	public static void main(String[] args) {
                 System.out.println("Init main spring boot ----------------------");
+                System.out.println("TEST OAUTH2 ---------------");
 		SpringApplication.run(SpringLoginApplication.class, args);
 	}
 
